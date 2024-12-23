31f0768e7f958ffa5ee8d621144e7ad8b3f5d15c
BugraKaraca <141683072+karacabugra27@users.noreply.github.com>
2024-12-23 23:01:41 +0300

    commit 18 - gereksiz bir method silindi

--- app/src/main/java/com/example/kelimekartlarim/StrategyPuanlama/StrategyManager.kt
+++ app/src/main/java/com/example/kelimekartlarim/StrategyPuanlama/StrategyManager.kt
@@ -1,10 +1,6 @@
 package com.example.kelimekartlarim.StrategyPuanlama
 
 class StrategyManager(private var strategy: IStrategyPuanlama) {
-    fun strategyBelirle(strategy : IStrategyPuanlama) {
-        this.strategy = strategy
-    }
-
     fun puanHesaplama(dogruSayisi: Int): Int {
         return strategy.puanHesapla(dogruSayisi)
     } //stratejiye yollayıp doğru sayısıyla puanını hesaplıyoruz.(zorluğa göre)
